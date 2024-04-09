c8fd766beb594ec75bc9130540f919bd0c84b031
Ahnabu <horairaabu013025@gmail.com>
2024-04-09 20:06:09 +0600

    card section updated

--- b9-a9-react/src/Components/Cards/Cards.jsx
+++ b9-a9-react/src/Components/Cards/Cards.jsx
@@ -10,7 +10,7 @@ const Cards = () => {
         <div className="mt-5 mx-auto ">
               <h1 className="text-3xl font-bold text-center">Chose Your Next Residence</h1>
             <p className="text-xl  text-center">Live well, dwell well. We empower you to find the perfect residence. Browse listings, explore neighborhoods, and access expert advice – all on DwellWell.</p>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-3 mx-auto">
+            <div className="grid grid-cols-1  gap-5 mt-3 mx-auto">
                 {
                     cards.map(card => <SingleCard key={card.id} card={card}></SingleCard>)
                 }
