ab32ca5622b8c23b6d2271062d180cd021035cc8
Ahnabu <horairaabu013025@gmail.com>
2024-04-12 20:14:32 +0600

    user profile sidebar

--- b9-a9-react/src/Components/UserProfile/UserProfile.jsx
+++ b9-a9-react/src/Components/UserProfile/UserProfile.jsx
@@ -5,14 +5,13 @@ import React from "react";
 import {
     Drawer,
     Button,
-    Typography,
-    IconButton,
+   
 } from "@material-tailwind/react";
 
-
-
+import { FaCircleUser } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 const UserProfile = () => {
-    const { LogOut } = useContext(AuthContext);
+    const { LogOut,user } = useContext(AuthContext);
     const [open, setOpen] = React.useState(false);
 
     const openDrawer = () => setOpen(true);
@@ -20,29 +19,27 @@ const UserProfile = () => {
     return (
         <div>
             <React.Fragment>
-                <Button onClick={openDrawer} className="w-12 h-12 rounded-full  bg-cover dark:bg-gray-500" style={{ backgroundImage: `url(https://source.unsplash.com/100x100/?portrait)`}}>
-                  
+                <Button onClick={openDrawer} className="w-12 h-12 rounded-full  dark:bg-gray-500" >
+                    <FaCircleUser className="text-5xl -mt-[14px] -ml-6"  />
                 </Button>
-                <Drawer open={open} onClose={closeDrawer} className="p-4">
-                    <div className="h-full p-3 space-y-2 w-60 dark:bg-gray-50 dark:text-gray-800">
+                <Drawer open={open} onClose={closeDrawer} className="p-4 ">
+                    <div className="h-full p-3 space-y-2 w-60 bg-white dark:text-gray-800  ">
                         <div className="flex items-center p-2 space-x-4">
 
                             <div>
-                                <h2 className="text-lg font-semibold">Leroy Jenkins</h2>
+                                <h2 className="text-lg font-semibold">{user.displayName}</h2>
                                 <span className="flex items-center space-x-1">
-                                    <a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-600">View profile</a>
+                                    {user?.photoURL ? <div className="w-12 h-12 rounded-full bg-cover" style={{ backgroundImage: `url(${user.photoURL})` }} ></div> :<FaCircleUser className="text-5xl -mt-[14px] -ml-6" />  }
                                 </span>
                             </div>
                         </div>
-                        <div className="divide-y dark:divide-gray-300">
-                            <ul className="pt-2 pb-4 space-y-1 text-sm">
-                                <li className="dark:bg-gray-100 dark:text-gray-900">
-                                    <a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current dark:text-gray-600">
-                                            <path d="M68.983,382.642l171.35,98.928a32.082,32.082,0,0,0,32,0l171.352-98.929a32.093,32.093,0,0,0,16-27.713V157.071a32.092,32.092,0,0,0-16-27.713L272.334,30.429a32.086,32.086,0,0,0-32,0L68.983,129.358a32.09,32.09,0,0,0-16,27.713V354.929A32.09,32.09,0,0,0,68.983,382.642ZM272.333,67.38l155.351,89.691V334.449L272.333,246.642ZM256.282,274.327l157.155,88.828-157.1,90.7L99.179,363.125ZM84.983,157.071,240.333,67.38v179.2L84.983,334.39Z"></path>
-                                        </svg>
-                                        <span>Dashboard</span>
-                                    </a>
+                        <div className="divide-y dark:divide-gray-300 bg-white bg-transparent  ">
+                            <ul className="pt-2 pb-4 space-y-1 text-sm ">
+                                <li className="dark:bg-gray-700 dark:text-gray-900 backdrop-blur-3xl">
+                                    <p rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+                                        <MdEmail />
+                                        <span>{user.email} </span>
+                                    </p>
                                 </li>
                                 <li>
                                     <a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
